e9e750106ce5255710e455e97fb33b318eccca0b
HanSeonmin <xeonmin0215@naver.com>
2019-06-27 22:07:22 +0900

    UPDATE MainActivity by xm

--- GoldenTicket/src/main/java/com/example/goldenticket/MainActivity.kt
+++ GoldenTicket/src/main/java/com/example/goldenticket/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    //update by seonmin!
+    //test by seonmin
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
