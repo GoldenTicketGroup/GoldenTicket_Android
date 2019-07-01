bfa2c1ef33cd26acc3575c2240bfb6ce5dff976c
xxxeonm <xeonmin0215@naver.com>
2019-07-02 01:07:36 +0900

    ADD LotteryConfirmActivity

---
+++ GoldenTicket/src/main/java/com/example/goldenticket/Data/LotteryConfirmData.kt
@@ -0,0 +1,12 @@
+package com.example.goldenticket.Data
+
+import java.util.*
+
+data class LotteryConfirmData (
+    var stageinfo_poster: String,
+    var stageinfo_date: String,
+    var stageinfo_title: String,
+    var stageinfo_detail: String,
+    var stageinfo_location: String,
+    var stageinfo_time: String
+)
