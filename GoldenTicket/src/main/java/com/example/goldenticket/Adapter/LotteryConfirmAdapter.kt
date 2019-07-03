486173500cfc8004927eaea85b21efd812188fa9
xxxeonm <xeonmin0215@naver.com>
2019-07-03 16:51:40 +0900

    UPDATE SearchActivity, ADD SearchResultActivity

--- GoldenTicket/src/main/java/com/example/goldenticket/Adapter/LotteryConfirmAdapter.kt
+++ GoldenTicket/src/main/java/com/example/goldenticket/Adapter/LotteryConfirmAdapter.kt
@@ -11,14 +11,14 @@ import kotlinx.android.synthetic.main.vp_lottery_confirm.view.*
 class LotteryConfirmAdapter(var inflater: LayoutInflater, val dataList: ArrayList<LotteryConfirmData>) : PagerAdapter(){
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        var view: View? = null
-        view = inflater.inflate(R.layout.vp_lottery_confirm, null)
-        view!!.tvShowName.text = dataList[position].title // TODO: 서버에서 받은 텍스트로 바꾸기
-        view!!.tvCheckText.visibility= View.VISIBLE
-        view!!.tvTime.text = dataList[position].time
-        container.addView(view)
-
-        return view
+          var view: View? = null
+          view = inflater.inflate(R.layout.vp_lottery_confirm, null)
+//        view!!.tvShowName.text = dataList[position].title // TODO: 서버에서 받은 텍스트로 바꾸기
+//        view!!.tvCheckText.visibility= View.VISIBLE
+//        view!!.tvTime.text = dataList[position].time
+//        container.addView(view)
+//
+          return view
     }
 
     override fun isViewFromObject(p0: View, p1: Any): Boolean {
