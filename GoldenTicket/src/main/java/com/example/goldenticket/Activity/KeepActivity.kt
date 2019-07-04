e4781e33f9b670d4765a257d9ba3ed6ff257a8d5
minkyoe <ddal0924@naver.com>
2019-07-05 01:46:02 +0900

    MOD KeepActivity

--- GoldenTicket/src/main/java/com/example/goldenticket/Activity/KeepActivity.kt
+++ GoldenTicket/src/main/java/com/example/goldenticket/Activity/KeepActivity.kt
@@ -48,27 +48,6 @@ class KeepActivity : AppCompatActivity() {
         rvKeepStage.addItemDecoration(RecyclerViewDecoration())
         rvKeepStage.setHasFixedSize(true)
     }
-
-//    override fun onCreateOptionsMenu(menu: Menu?): kotlin.Boolean {
-//        val actionBar = supportActionBar
-//
-//        actionBar!!.setDisplayShowCustomEnabled(true)
-//        actionBar.setDisplayHomeAsUpEnabled(false) // 액션바 아이콘을 업 네비게이션 형태로 표시
-//        actionBar.setDisplayShowTitleEnabled(false) // 액션바에 표시되는 제목의 표시유무 설정
-//        actionBar.setDisplayShowHomeEnabled(false) // 홈 아이콘 숨김처리
-//        actionBar.elevation="0".toFloat()
-//
-//
-//        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-//        val actionbar = inflater.inflate(R.layout.keep_custom_actionbar, null)
-//
-//        actionBar.setCustomView(actionbar)
-//
-//        //액션바 양쪽 공백 없애기
-//        val parent = actionbar.getParent() as androidx.appcompat.widget.Toolbar
-//        parent.setContentInsetsAbsolute(0, 0)
-//
-//        return true
-//    }
+    
 
 }
