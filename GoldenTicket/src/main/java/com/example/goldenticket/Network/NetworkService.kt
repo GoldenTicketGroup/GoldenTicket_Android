b62da151b5f345041f65d1c0c9c6271f7144af71
ganghee <idiosauri@naver.com>
2019-07-06 13:31:21 +0900

    update 서버와의 연결

--- GoldenTicket/src/main/java/com/example/goldenticket/Network/NetworkService.kt
+++ GoldenTicket/src/main/java/com/example/goldenticket/Network/NetworkService.kt
@@ -1,6 +1,7 @@
 package com.example.goldenticket.Network
 
 import com.example.goldenticket.Network.Post.PostLoginResponse
+import com.example.goldenticket.Network.Post.PostSignupResponse
 import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.http.Body
@@ -10,9 +11,18 @@ import retrofit2.http.POST
 interface NetworkService {
     //post방식은 HTTP Request의 Body에 Json 포맷으로 데이터를 담아서 전달.
     //로그인 api
-    @POST("/api/auth/signin")
+    @POST("/auth/signin")
     fun postLoginResponse(
         @Header("Content-Type") content_type: String,
         @Body body: JsonObject
     ): Call<PostLoginResponse>
+
+    //회원가입 api
+    @POST("/auth/signup")
+    fun postSignupResponse(
+        @Header("Content-Type") content_type: String,
+        @Body body: JsonObject
+    ): Call<PostSignupResponse>
+
+
 }
--- GoldenTicket/src/main/java/com/example/goldenticket/Network/Post/PostSignupResponse.kt
+++ GoldenTicket/src/main/java/com/example/goldenticket/Network/Post/PostSignupResponse.kt
@@ -1,2 +1,8 @@
 package com.example.goldenticket.Network.Post
 
+data class PostSignupResponse (
+    val status: Int,        //status상태 코드 예)200,404
+    val success: Boolean,   //성공여부
+    val message:String,     //사용자를 위한 안내 메시지
+    val data:String?        //사용자 인증 정보를 저장할 토큰값 null값을 가질 수 있다.
+)
